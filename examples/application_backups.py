fea9cf1e50c84151ec0e034df18c84540f42d611
Ashutosh Kulkarni <ashutoshk@vmware.com>
2019-05-16 13:05:51 +0530

    Backup restore of applications and tiers

---
+++ examples/application_backups.py
@@ -0,0 +1,77 @@
+# Python SDK Examples
+# Script will get total bytes transferred for a specific IP or group of IPs
+# or the scope can be any l2 network, security group, etc.
+
+import init_api_client
+import argparse
+import swagger_client
+import utilities
+import logging
+import yaml
+
+logger = logging.getLogger("vrni_sdk")
+
+
+def main(args):
+    application_api = swagger_client.ApplicationsApi()
+
+    if args.application_backup_action == 'save':
+        all_apps = []
+        params = dict(size=10)
+        while True:
+            apps = application_api.list_applications(**params)
+            for i in apps.results:
+                app = application_api.get_application(i.entity_id)
+                logger.info("Getting application '{}'".format(app.name))
+                tiers = application_api.list_application_tiers(id=app.entity_id)
+                app_to_save = dict(name=app.name, no_of_tiers=len(tiers.results), tiers=tiers.to_dict())
+                all_apps.append(app_to_save)
+            if not apps.cursor:
+                break
+            params['cursor'] = apps.cursor
+
+        logger.info("Storing Application/Tier info to {}".format(args.application_backup_csv))
+        with open(args.application_backup_csv, 'w') as outfile:
+            yaml.dump(all_apps, outfile, default_flow_style=False)
+
+    if args.application_backup_action == 'restore':
+        with open(args.application_backup_csv, 'r') as outfile:
+            all_apps = yaml.load(outfile)
+
+        for app in all_apps:
+            logger.info("Restoring app '{}'".format(app['name']))
+            body = {"name": app['name'] + '-restored'}
+            created_application = application_api.add_application(body)
+            for tier in app['tiers']['results']:
+                tier.pop('application')
+                tier.pop('entity_id')
+                logger.info("\tRestoring tier '{}'".format(tier['name']))
+                application_api.add_tier(created_application.entity_id, tier)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Run Public APIs on vRNI Platform')
+    parser.add_argument('--platform_ip', action='store',
+                        help='IP address of vRNI platform. In case of cluster IP address of Platform-1')
+    parser.add_argument('--username', action='store', default='admin@local',
+                        help='user name for authentication')
+    parser.add_argument("--password", action="store",
+                        default='admin', help="password for authentication")
+    parser.add_argument("--domain_type", action="store",
+                        default='LOCAL', help="domain type for authentication")
+    parser.add_argument("--data_sources_csv", action="store",
+                        default='data_sources.csv', help="Data sources are saved in this csv")
+    parser.add_argument("--application_backup_csv", action="store",
+                        default='application_backup.yml', help="Applications and tiers are saved in this csv")
+    parser.add_argument("--application_backup_action", action="store",
+                        default='save', help="Action can be 'save' or 'restore'")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    utilities.configure_logging("/tmp")
+    api_client = init_api_client.get_api_client(args)
+    main(args)
